ae0f8a6f880a7ead437a8bd34b1d826a36d15449
Lucas <lucasidelfonso73@gmail.com>
2022-12-27 18:53:24 -0300

    Eliminando arquivos desnecessários

--- utf8Analysing.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-with open ("DataSet/data.csv","r",encoding="utf-8") as f:
-    for i in f:
-            for c in i:
-                 if(ord(c)<128):
-                     print(c,end=",")
